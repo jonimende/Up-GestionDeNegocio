a0651171e04f880a7ade2e305544cac9b51dd77a
Franco de Iriondo <91231311+Knd0@users.noreply.github.com>
2025-08-13 19:07:46 -0300

    Update db.ts

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -5,4 +5,10 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Evita problemas con certificados auto-firmados
+    }
+  }
 });
